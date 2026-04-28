304649fce511182a78b62761717bb3d816ad9c48
yara22aulia <yara22aulia@gmail.com>
2026-04-28 15:43:27 +0700

    inisialisasi program

---
+++ friendClassSatu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class siswa;
+class orang
+{
+private:
+    string nama;
+
+public:
+    void setNama(string pNama);
+    friend class siswa;
+};
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
+int main()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
