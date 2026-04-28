954cecc6c2662c3181a4f5717a55cb2e12e09acd
yara22aulia <yara22aulia@gmail.com>
2026-04-28 15:20:27 +0700

    first commit

---
+++ poinSatu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout<<"No Induk = "<<nim<<endl;
+    }
+};
+
+int main (){
+    mahasiswa mhs{1}; // Object mhs
+    mhs.showNim(); //Member Access Operator
+
+    mahasiswa &refMhs = mhs; //Pointer Reference refMhs
+    refMhs.nim = 2; //Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; //pointer Dereference pMhs
+    pMhs->nim = 3; //Arrow Operator
+    pMhs->showNim();
+    return 0;
+}
