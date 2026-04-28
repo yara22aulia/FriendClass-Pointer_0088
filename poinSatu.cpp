9a1c40141f1b078378c1b6078853ed4a136ea166
yara22aulia <yara22aulia@gmail.com>
2026-04-28 15:35:41 +0700

    menambahkan method showNim

--- poinSatu.cpp
+++ poinSatu.cpp
@@ -4,7 +4,7 @@ class mahasiswa{
     public:
     int nim;
     void showNim(
-        
+
     )
     {
         cout<<"No Induk = "<<nim<<endl;
@@ -13,7 +13,9 @@ class mahasiswa{
 
 int main (){
     mahasiswa mhs{1}; // Object mhs
-    mhs.showNim(); //Member Access Operator
+    mhs.showNim(
+        
+    ); //Member Access Operator
 
     mahasiswa &refMhs = mhs; //Pointer Reference refMhs
     refMhs.nim = 2; //Member Access Operator
