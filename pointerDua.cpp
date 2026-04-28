0dcb4293a2338829d25bfaf5c6b1c88fe58dc0df
yara22aulia <yara22aulia@gmail.com>
2026-04-28 15:23:46 +0700

    inisialisasi program dasar

---
+++ pointerDua.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class mahasiswa
+{
+public:
+    int nim;
+    void showNim()
+    {
+        cout << "No Induk = " << nim << endl;
+    }
+};
+
+int main()
+{
+    mahasiswa *mhs = new mahasiswa(); // pointer Object mhs
+    mhs->nim = 2;
+    mhs->showNim();
+    delete mhs;
+    return 0;
+}
